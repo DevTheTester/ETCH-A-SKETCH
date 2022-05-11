613ed2255ef20c61d4e80e2c0ce649ecfa30a397
Devon <dev.kahika.campbell@gmail.com>
2022-05-12 05:23:09 +1200

    Add clear button code that isn't working for review

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,13 @@
 // Get grid container as variable, create grid item so we can add using for loop
 let gridContainer = document.querySelector('.grid-container');
+let clearButton = document.querySelector('.clear');
+let resizeButton = document.querySelector('.resize');
 
 // Grid container is set up to divide whole page 16x16 for loop will add that amount of items
 for (let i = 0; i < 16 * 16; i++) {
-   let gridItem = document.createElement('div')
-    gridItem.classList.add('grid-item')
-    gridContainer.appendChild(gridItem)
+  let gridItem = document.createElement('div')
+  gridItem.classList.add('grid-item')
+  gridContainer.appendChild(gridItem)
 };
 
 let allGridItems = document.querySelectorAll('.grid-item');
@@ -14,7 +16,11 @@ let allGridItems = document.querySelectorAll('.grid-item');
 // and change back when mouse isn't over
 
 allGridItems.forEach(item => 
-    item.addEventListener('mouseover', () =>
-    item.style.backgroundColor = 'black' ));
+  item.addEventListener('mouseover', () =>
+  item.style.backgroundColor = 'black' ));
+
+clearButton.addEventListener('click', () =>
+  allGridItems.forEach(item =>
+    item.backgroundColor = 'rgb(177, 229, 231)'));
 
 
